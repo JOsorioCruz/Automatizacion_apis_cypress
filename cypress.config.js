824e6ecaeacd147f3656a666640c0f00d493f6c1
JOsorioCruz <josorioc@unicartagena.edu.co>
2024-11-01 00:18:57 -0500

    Se mejoro el sistema de evidencias

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  video: false,
+  video: true,
   screenshotOnRunFailure: false,
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
