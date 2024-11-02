83844f522f0770ac4e1358a1965f9790bd3c4ba4
JOsorioCruz <josorioc@unicartagena.edu.co>
2024-11-01 19:24:35 -0500

    Realizando falla intencional para verificar la generacion del artifact

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const  {configureAllureAdapterPlugins} = require ('@mmisty/cypress-allure-adapte
 
 module.exports = defineConfig({
   retries: 1,
-  video: false,
+  video: true,
   screenshotOnRunFailure: true,
   e2e: {
     baseUrl: 'https://petstore.swagger.io/v2',
